075582823746563cfdd2fa7e8ff92568d00d4afa
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-07-02 10:37:56 +0300

    Update SWtimer.php

--- SW/src/svile/sw/SWtimer.php
+++ SW/src/svile/sw/SWtimer.php
@@ -40,34 +40,30 @@
 
 namespace svile\sw;
 
+use pocketmine\scheduler\Task;
 
-use pocketmine\scheduler\PluginTask;
-
-
-class SWtimer extends PluginTask
+class SWtimer extends Task
 {
-    /** @var int */
-    private $seconds = 0;
-    /** @var bool */
-    private $tick = false;
 
+    /** @var bool */
+    private $tick;
 
     public function __construct(SWmain $plugin)
     {
         parent::__construct($plugin);
-        $this->tick = (bool)$plugin->configs['sign.tick'];
+        $this->tick = (bool) $plugin->configs["sign.tick"];
     }
 
-
-    public function onRun($tick)
+    public function onRun(int $tick) : void
     {
-        foreach ($this->getOwner()->arenas as $SWname => $SWarena)
-            $SWarena->tick();
+        $owner = $this->getOwner();
 
-        if ($this->tick) {
-            if (($this->seconds % 5 == 0))
-                $this->getOwner()->refreshSigns();
-            $this->seconds++;
+        foreach ($owner->arenas as $arena) {
+            $arena->tick();
+        }
+
+        if ($this->tick && ($tick % 5 === 0)) {
+            $owner->refreshSigns();
         }
     }
 }
